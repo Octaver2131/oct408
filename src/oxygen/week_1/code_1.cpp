8f1b4d6a8aadd8d6e6bc6c21a6843b6fd7f4b98e
Octaver <Octaver2131@outlook.com>
2026-06-14 00:37:13 +0800

    feat: 9.回文数

---
+++ src/oxygen/week_1/code_1.cpp
@@ -0,0 +1,44 @@
+// LeetCode: https://leetcode.cn/problems/palindrome-number/description/
+
+// oxygen_code
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        else if (x == 0) return true;
+        else {
+            long long res = 0;
+            int t = x;
+            while (x) {
+                res = res * 10;
+                res = res + x % 10;
+                x = x / 10;
+            }
+            if (res == t) return true;
+            return false;
+        }
+    }
+};
+
+// octaver_code
+using ll = long long;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x < 0) return false;
+        else if (x == 0) return true;
+        else
+        {
+            ll res = 0;
+            int t = x;
+            while (x) {
+                res *= 10;
+                res += x % 10;
+                x /= 10;
+            }
+            return res == t;
+        }
+    }
+};
+
