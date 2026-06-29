85bc2e380a764f9206ace3f620c5dd07f9c6b943
Octaver <Octaver2131@outlook.com>
2026-06-29 22:24:17 +0800

    feat: 704. 二分查找

--- src/oxygen/week_4/code_1.cpp
+++ src/oxygen/week_4/code_1.cpp
@@ -1,6 +1,20 @@
-// LeetCode:
+// LeetCode: https://leetcode.cn/problems/binary-search/description/
 
 // oxygen_code
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
+        while(l <= r) {
+            int mid = (l + r) >> 1;
+            if (nums[mid] > target) r = mid - 1;
+            else if (nums[mid] < target) l = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+};
 
 // octaver_code
 
