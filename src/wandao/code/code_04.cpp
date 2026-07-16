0d52256c996c01133d95e5970109d932cafc8dd8
Octaver <Octaver2131@outlook.com>
2026-07-16 08:38:57 +0800

    feat: wandao - 单链表 - 不带头结点

---
+++ src/wandao/code/code_04.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+
+/*
+ * 单链表 - 不带头结点
+ */
+
+typedef struct LNode {
+    ElemType data;
+    struct LNode *next;
+} LNode, *LinkList;
+
+// 单链表的初始化
+bool InitList(LinkList &L) {
+    L = (LNode*)malloc(sizeof(LNode));
+    L->next = NULL;
+    return true;
+}
