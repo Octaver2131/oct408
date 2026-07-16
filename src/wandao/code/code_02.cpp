25ec403508a8fd03f085f245e17413ee741d1c17
Octaver <Octaver2131@outlook.com>
2026-07-16 08:38:25 +0800

    feat: wandao - 顺序表 - 静态分配

---
+++ src/wandao/code/code_02.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+
+/*
+ * 顺序表 - 静态分配
+ */
+
+#define InitSize 100
+
+typedef struct {
+    ElemType *data;
+    int MaxSize, length;
+} SeqList;
+
+// C 的初始动态分配语句为
+// L.data = (ElemType*)malloc(sizeof(ElemType) * InitSize);
+
+// C++的初始动态分配语句为
+// L.data = new ElemType[InitSize];
+
+// 顺序表的初始化
+void InitList(SeqList &L){
+    L.data = (ElemType*)malloc(InitSize * sizeof(ElemType));
+    L.length = 0;
+    L.MaxSize = InitSize;
+}
+
