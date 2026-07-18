b789a66e5c549db4f092faa3a8f028d4196ff19c
Octaver <Octaver2131@outlook.com>
2026-07-18 15:02:20 +0800

    feat: wandao - 静态链表

---
+++ src/wandao/code/code_06.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+
+/*
+ * 静态链表
+ */
+
+#define MaxSize 50
+
+typedef struct {
+    ElemType data;
+    int next;
+} SLinkList[MaxSize];
