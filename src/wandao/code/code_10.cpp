6748ca9d0549a3f6678eec0f00d2ffe5fcf3710b
Octaver <Octaver2131@outlook.com>
2026-07-18 15:05:44 +0800

    feat: wandao - 队列的链式存储

---
+++ src/wandao/code/code_10.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int ElemType;
+
+/*
+ * 队列的链式存储
+ */
+
+typedef struct LinkNode {
+    ElemType data;
+    struct LinkNode *next;
+} LinkNode;
+
+typedef struct {
+    LinkNode *front, *rear;
+} LinkQueue;
+
+// 初始化
+void InitQueue(LinkQueue &Q) {
+    Q.front = Q.rear = (LinkNode*)malloc(sizeof(LinkNode));
+    Q.front->next = NULL;
+}
+
+// 判队空
+bool QueueEmpty(LinkQueue Q) {
+    if (Q.front == Q.rear)
+        return true;
+    else
+        return false;
+}
+
+// 入队
+void EnQueue(LinkQueue &Q, ElemType x) {
+    LinkNode *s=(LinkNode *)malloc(sizeof(LinkNode));
+    s->data = x;
+    s->next = NULL;
+    Q.rear->next = s;
+    Q.rear = s;
+}
+
+// 出队
+bool DeQueue(LinkQueue &Q, ElemType &x) {
+    if (Q.front == Q.rear)
+        return false;
+    LinkNode *p = Q.front->next;
+    x = p->data;
+    Q.front->next = p->next;
+    if (Q.rear == p)
+        Q.rear = Q.front;
+    free(p);
+    return true;
+}
