2df1a78829ad8ae4c3838f8a101d802954c0b4de
Octaver <Octaver2131@outlook.com>
2026-08-03 13:21:50 +0800

    feat: wandao - 记录形信号量

---
+++ src/wandao/OS/sync_mutex_sem/sem_02.cpp
@@ -0,0 +1,28 @@
+#if 0
+
+/*
+ * 记录形信号量
+ */
+
+typedef struct {
+    int value;
+    struct process *L;
+} semaphore;
+
+void wait(semaphore S) {     // 相当于申请资源 - P 操作
+    S.value--;
+    if (S.value < 0) {
+        add this process to S.L;
+        block(S.L);
+    }
+}
+
+void signal(semaphore S) {   // 相当于释放资源 - V 操作
+    S.value++;
+    if (S.value <= 0) {
+        remove a process P from S.L;
+        wakeup(P);
+    }
+}
+
+#endif
