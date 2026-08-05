b8e336ae187e3ae59c383aaf98e407d21f95a2be
Octaver <Octaver2131@outlook.com>
2026-08-05 09:05:19 +0800

    feat: wandao - 折半查找

---
+++ src/wandao/DS/xianyu/code_22.cpp
@@ -0,0 +1,27 @@
+#if 0
+
+/*
+ * 折半查找
+ */
+
+typedef struct {
+    ElemType *elem;
+    int TableLen;
+} SSTable;
+
+// 折半查找
+int Binary_Search(SSTable L, ElemType key) {
+    int low = 0, high = L.TableLen - 1, mid;
+    while (low <= high) {
+        mid = (low + high) / 2;
+        if (L.elem[mid] == key)
+            return mid;
+        else if(L.elem[mid] > key)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return -1;
+}
+
+#endif
