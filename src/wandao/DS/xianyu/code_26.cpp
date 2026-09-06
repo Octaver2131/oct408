1b63dc32f18260f6fccd4756f5e3358083ef3168
Octaver <Octaver2131@outlook.com>
2026-09-07 07:57:41 +0800

    feat: wandao - 希尔插入排序

---
+++ src/wandao/DS/xianyu/code_26.cpp
@@ -0,0 +1,20 @@
+#if 0
+
+/*
+ * 希尔插入排序
+ */
+
+// 希尔排序
+void ShellSort(ElemType A[], int n) {
+    int dk,i,j;
+    for (dk = n / 2; dk >= 1; dk = dk / 2)
+        for (i = dk + 1; i <= n; ++i)
+            if (A[i] < A[i - dk]) {
+                A[0] = A[i];
+                for (j = i - dk ; j > 0 && A[0] < A[j]; j -= dk)
+                    A[j + dk] = A[j];
+                A[j + dk] = A[0];
+            }
+}
+
+#endif
