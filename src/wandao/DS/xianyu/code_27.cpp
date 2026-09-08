f9114611099757d2276340734af7f00ee26948a5
Octaver <Octaver2131@outlook.com>
2026-09-08 09:33:34 +0800

    feat: wandao - 冒泡交换排序

---
+++ src/wandao/DS/xianyu/code_27.cpp
@@ -0,0 +1,21 @@
+#if 0
+
+/*
+ * 冒泡交换排序
+ */
+
+// 冒泡排序
+void BubbleSort(ElemType A[], int n) {
+    for (int i = 0;i < n - 1; i++) {
+        bool flag = false;
+        for (int j = n - 1; j > i; j--)
+            if (A[j - 1] > A[j]) {
+                swap(A[j - 1], A[j]);
+                flag = true;
+            }
+        if (flag == false)
+            return;
+    }
+}
+
+#endif
